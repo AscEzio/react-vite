0fdc18b5a391fd607513ed1342a4097396e1dd79
AscEzio <1647638002@qq.com>
2025-01-16 11:12:37 +0800

    style: code format

--- vite.config.ts
+++ vite.config.ts
@@ -4,36 +4,36 @@ import path from 'path'
 import { generateScopedName } from './scripts/generateScopedName'
 
 export default ({ mode }: UserConfig) => {
-    return defineConfig({
-        plugins: [
-            react()
-        ],
-        define: {
-            'process.env.NODE_ENV': `"${mode as string}"`
-        },
-        css: {
-            modules: {
-                generateScopedName,
-                localsConvention: 'camelCase'
-            },
-            preprocessorOptions: {
-                less: {
-                    math: 'always'
-                }
-            }
-        },
-        resolve: {
-            alias: [
-                {
-                    find: /^~/,
-                    replacement: ''
-                },
-                { find: '@', replacement: path.resolve(__dirname, 'src') }
-            ]
-        },
-        base: './',
-        server: {
-            port: 3050
+  return defineConfig({
+    plugins: [
+      react()
+    ],
+    define: {
+      'process.env.NODE_ENV': `"${mode as string}"`
+    },
+    css: {
+      modules: {
+        generateScopedName,
+        localsConvention: 'camelCase'
+      },
+      preprocessorOptions: {
+        less: {
+          math: 'always'
         }
-    })
+      }
+    },
+    resolve: {
+      alias: [
+        {
+          find: /^~/,
+          replacement: ''
+        },
+        { find: '@', replacement: path.resolve(__dirname, 'src') }
+      ]
+    },
+    base: './',
+    server: {
+      port: 3050
+    }
+  })
 }
